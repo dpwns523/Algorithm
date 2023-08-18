912dd2e7a09e4132746785b1c9e1497f25e7791d
dpwns523 <84260096+dpwns523@users.noreply.github.com>
2023-08-18 11:48:08 +0900

    [Gold IV] Title: 비밀 모임, Time: 304 ms, Memory: 21304 KB -BaekjoonHub

--- 백준/Gold/13424. 비밀 모임/비밀 모임.java
+++ 백준/Gold/13424. 비밀 모임/비밀 모임.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int test_case = 0; test_case < T; test_case++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
@@ -24,34 +25,43 @@ public class Main {
                 map[b][a] = c;
             }
 
-            for (int k = 1; k <= n; k++) {  // 최단경로 업데이트
-                for (int i = 1; i <= n; i++) {
-                    for (int j = 1; j <= n; j++) {
-                        map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
-                    }
-                }
-            }
+            floydWarshall(n);   // 최단 경로 업데이트
 
             int k = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
             int[] friends = new int[k];
-            for (int i = 0; i < k; i++) {   // 모임에 오는 친구입력
+            for (int i = 0; i < k; i++) {   // 모임에 오는 친구
                 friends[i] = Integer.parseInt(st.nextToken());
             }
-            int min = Integer.MAX_VALUE, minIdx = 0;
-            for (int i = 1; i <= n; i++) {  // 최소 비용으로 모두 모이는 경우 탐색
-                int cost = 0;
-                for (int j = 0; j < k; j++) {
-                    cost += map[friends[j]][i];
-                }
-                if(min > cost) {
-                    min = cost;
-                    minIdx = i;
-                }
-            }
-            System.out.println(minIdx);
+           sb.append(findMinLocation(n, friends)).append("\n");
+
         }
+        System.out.print(sb);
         br.close();
     }
 
+    public static void floydWarshall(int n) {
+        for (int k = 1; k <= n; k++) {  // 최단경로 업데이트
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+                }
+            }
+        }
+    }
+
+    public static int findMinLocation(int n, int[] friends) {
+        int min = Integer.MAX_VALUE, minIdx = 0;
+        for (int i = 1; i <= n; i++) {  // 최소 비용으로 모두 모이는 경우 탐색
+            int cost = 0;
+            for (int j = 0; j < friends.length; j++) {
+                cost += map[friends[j]][i];
+            }
+            if(min > cost) {
+                min = cost;
+                minIdx = i;
+            }
+        }
+        return minIdx;
+    }
 }
