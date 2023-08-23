1a28061dc99c16b4d1f2d39455fddd82241868c7
dpwns523 <84260096+dpwns523@users.noreply.github.com>
2023-08-23 09:42:19 +0900

    [Silver I] Title: Small World Network, Time: 772 ms, Memory: 15664 KB -BaekjoonHub

--- 백준/Silver/18243. Small World Network/Small World Network.java
+++ 백준/Silver/18243. Small World Network/Small World Network.java
@@ -1,38 +1,81 @@
-import java.io.*;
-import java.util.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static final int MAX_VALUE = 101;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
 
-        int[][] graph = new int[n+1][n+1];
+	private static Node[] nodes;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if(i==j) continue;
-                graph[i][j] = MAX_VALUE;
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int n=Integer.parseInt(st.nextToken());
+		int k=Integer.parseInt(st.nextToken());
+		
+		nodes = new Node[n+1];
+		
+		for(int i=1;i<=n;i++) {
+			nodes[i]=new Node(null,i);
+		}
+		
+		for(int i=0;i<k;i++) {
+			st=new StringTokenizer(br.readLine());
+			
+			int a=Integer.parseInt(st.nextToken());
+			int b=Integer.parseInt(st.nextToken());
+			
+			nodes[a].next=new Node(nodes[a].next,b);
+			nodes[b].next=new Node(nodes[b].next,a);	
+		}//입력 종료
+		boolean[] flag;
+		int[] dist;
+		for(int i=1;i<=n;i++) {
+			flag=new boolean[n+1];
+			dist=new int[n+1];
+			Arrays.fill(dist, 1000);
+			dist[i]=0;
+			dfs(i,dist,0,flag);
+			
+			for(int j=1;j<=n;j++) {
+				if(dist[j]>=7) {
+					System.out.println("Big World!");
+					return;
+				}
+			}
+		}
+		System.out.println("Small World!");
+		
+		
+	}//end of main
+	private static void dfs(int start,int[] dist,int cur,boolean[] flag) {
+		if(dist[start]>cur) {
+			dist[start]=cur;
+		}
+		flag[start]=true;
 
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
-            graph[a][b] = 1;
-            graph[b][a] = 1;
-        }
+		for(Node next=nodes[start].next;next!=null;next=next.next) {
+			if(!flag[next.nodeNum]||dist[next.nodeNum]>cur+1) {
+				dfs(next.nodeNum,dist,cur+1,flag);
+			}
+		}
+	}
+	
+	
+	
+	
+	static class Node{
+		Node next;
+		int nodeNum;
+		
+		public Node(Node next, int nodeNum) {
+			this.next = next;
+			this.nodeNum = nodeNum;
+		}
+		
+	}
 
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
-                }
-            }
-        }
-        System.out.println(Arrays.stream(graph).flatMapToInt(Arrays::stream).anyMatch(v -> v>6) ? "Big World!" : "Small World!");
-        br.close();
-    }
 }
