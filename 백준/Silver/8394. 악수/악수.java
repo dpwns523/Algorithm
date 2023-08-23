159a7bda41ded35e457a27e825fa759b94f46540
dpwns523 <84260096+dpwns523@users.noreply.github.com>
2023-08-23 09:20:36 +0900

    [Silver III] Title: 악수, Time: 168 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/8394. 악수/악수.java
+++ 백준/Silver/8394. 악수/악수.java
@@ -1,17 +1,30 @@
-import java.io.*;
-import java.util.*;
 
-class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i-1] + dp[i-2]) % 10;
-        }
-        System.out.println(dp[n]);
-        br.close();
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		long before1 = 1;// n==2일때 값
+		long before2 = 2;// n==3일때 값
+
+		long tmp;
+
+		if (n == 1)
+			System.out.println(0);
+		else {
+			n = n - 2;
+
+			while (n-- > 0) {
+				tmp = (before1 + before2) % 10;
+				before1 = before2 % 10;
+				before2 = tmp;
+			}
+
+			System.out.println(before2);
+		}
+	}
 }
