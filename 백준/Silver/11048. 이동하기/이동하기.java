383b9259f57fa8e2a0857a32c6b72b8d36d37ae0
dpwns523 <84260096+dpwns523@users.noreply.github.com>
2023-08-30 14:59:15 +0900

    [Silver II] Title: 이동하기, Time: 716 ms, Memory: 104884 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -4,51 +4,22 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static int row, col;
-    public static int[][] map, direction = {{1,0}, {0,1}};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        row = Integer.parseInt(st.nextToken());
-        col = Integer.parseInt(st.nextToken());
-        map = new int[row][col];
+        int row = Integer.parseInt(st.nextToken()), col = Integer.parseInt(st.nextToken());
+        int[][] map = new int[row][col], dp = new int[row+1][col+1];
+
         for (int i = 0; i < row; i++) {
             map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
-
-        System.out.println(bfs());
-
-        br.close();
-    }
-
-    public static int bfs() {
-        Deque<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{0, 0});
-        int[][] res = new int[row][col];
-        int[][] visited = new int[row][col];
-        res[0][0] = map[0][0];
-        while (!queue.isEmpty()) {
-            int[] pos = queue.poll();
-            for (int i = 0; i < direction.length; i++) {
-                int nextR = pos[0] + direction[i][0];
-                int nextC = pos[1] + direction[i][1];
-                if(checkPos(nextR, nextC)) {
-                    int next = res[pos[0]][pos[1]] + map[nextR][nextC];
-                    if(next > res[nextR][nextC] || (next == res[nextR][nextC] && visited[nextR][nextC] == 0)) {
-                        // 최댓값 갱신 or 0인 곳 방문
-                        visited[nextR][nextC] = 1;  // 재방문 x
-                        res[nextR][nextC] = next;
-                        queue.offer(new int[]{nextR, nextC});
-                    }
-                }
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                dp[i][j] = map[i-1][j-1] + Math.max(dp[i-1][j], dp[i][j-1]);
             }
         }
-        return res[row-1][col-1];
-
-    }
-
-    public static boolean checkPos(int r, int c) {
-        return (r<0 || r>=row || c<0 || c>=col) ? false : true;
+        System.out.println(dp[row][col]);
+        br.close();
     }
 
 }
